94ca9b521b8a19197e7248606d5ce8cff3728465
Rajeev Pandey <rajeev.p.pandey@oracle.com>
2021-06-01 20:14:16 +0530

    First Commit Weather APP BI

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>BuildIt Weather App 5 day Forecast</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
